bc8f252f8f191953b416a249409044487e2af18e
Brian <bstanton773@gmail.com>
2024-01-26 15:15:02 -0600

    Add a Create Note route + add CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 import os
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+CORS(app)
 
 db = SQLAlchemy(app)
 
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import request
 from app import app, db
 from app.models import Note
 
@@ -6,3 +7,14 @@ from app.models import Note
 def get_notes():
     notes = db.session.execute(db.select(Note)).scalars().all()
     return [n.to_dict() for n in notes]
+
+
+@app.route('/notes', methods=['POST'])
+def create_note():
+    if not request.is_json:
+        return {'error': 'Your request Content-Type must be application/json'}, 400
+    data = request.json
+    if 'body' not in data:
+        return {'error': 'You must have "body" in your request body'}, 400
+    new_note = Note(body=data['body'])
+    return new_note.to_dict(), 201
